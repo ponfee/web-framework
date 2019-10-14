40906f3ad2268826422615f78167e3508e434446
Ponfee <ponfee.cn@gmail.com>
2019-10-14 19:08:38 +0800

    optimization

--- web-framework-demo/src/main/java/cn/ponfee/web/framework/controller/DownloadController.java
+++ web-framework-demo/src/main/java/cn/ponfee/web/framework/controller/DownloadController.java
@@ -1,5 +1,6 @@
 package cn.ponfee.web.framework.controller;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -7,20 +8,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import code.ponfee.commons.http.ContentType;
+import code.ponfee.commons.util.Captchas;
 import code.ponfee.commons.web.WebUtils;
 
 @Controller
 @RequestMapping("/download")
 public class DownloadController {
 
-    @RequestMapping("/normal")
-    public void normal(HttpServletResponse resp) throws FileNotFoundException {
-        WebUtils.response(resp, new FileInputStream("d:/procyon-decompiler-0.5.30.jar"), "test.zip");
+    @RequestMapping("/file")
+    public void normal(HttpServletResponse resp, @RequestParam(value = "isGzip", defaultValue = "false") boolean isGzip)
+        throws FileNotFoundException {
+        WebUtils.response(resp, new FileInputStream("d:/pushlet-userunicast.jar"), "长连接包.zip", "UTF-8", isGzip);
     }
 
-    @RequestMapping("/gzip")
-    public void gzip(HttpServletResponse resp) throws FileNotFoundException {
-        WebUtils.response(resp, new FileInputStream("d:/procyon-decompiler-0.5.30.jar"), "test.zip", "UTF-8", true);
+    @RequestMapping("/image")
+    public void image(HttpServletResponse resp, @RequestParam(value = "isGzip", defaultValue = "false") boolean isGzip) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Captchas.generate(80, out, "123456");
+        WebUtils.response(resp, out.toByteArray(), ContentType.IMAGE_JPEG.value(), false);
     }
 }
